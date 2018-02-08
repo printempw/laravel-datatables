1f27c392178e0f1f6943d7534af25badfc59fd4e
printempw <h@prinzeugen.net>
2018-02-09 02:19:07 +0800

    Fix count() to support PHP 7.2

--- src/Engines/CollectionEngine.php
+++ src/Engines/CollectionEngine.php
@@ -174,7 +174,8 @@ class CollectionEngine extends BaseEngine
     public function columnSearch()
     {
         $columns = $this->request->get('columns');
-        for ($i = 0, $c = count($columns); $i < $c; $i++) {
+        // Quick fix for supporting PHP 7.2
+        for ($i = 0, $c = (is_null($columns) ? 0 : count($columns)); $i < $c; $i++) {
             if ($this->request->isColumnSearchable($i)) {
                 $this->isFilterApplied = true;
                 $regex = $this->request->isRegex($i);
